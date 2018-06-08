37edd8372c9538a5be450a8dfb9d1fe282dc2b89
Psilon <alexpsilon@gmail.com>
2018-06-08 13:27:59 +0300

    Added slug

--- src/extension.js
+++ src/extension.js
@@ -3,11 +3,30 @@ const vscode = require('vscode');
 const Commands = vscode.commands;
 const Window = vscode.window;
 const symbolsCyrToLat = require('./symbols-cyr-to-lat');
+const { toArray } = require('./helpers/array');
+const wordsExceptios = require('./dictonaries');
 
-const toArray = context => Array.prototype.slice.call(context);
+const converter = (text, converTable) => {
+  const words = text
+    .trim()
+    .split(/[\s\.,!\?\(\)\[\]\{\}<>@#$%^&*;:'"`]/)
+    .map(word => {
+      if (typeof wordsExceptios[word] !== 'undefined') {
+        return wordsExceptios[word];
+      }
 
-const converter = (text, converTable) =>
-	toArray(text).map(symbol => (!(symbol in converTable) ? symbol : converTable[symbol])).join('');
+      return word;
+    });
+
+  return toArray(words.join(' '))
+    .map(symbol => (!(symbol in converTable) ? symbol : converTable[symbol]))
+    .join('');
+};
+
+const strToSlug = str => {
+  const strTranslit = converter(str, symbolsCyrToLat).toLowerCase();
+  return strTranslit.replace(/[^a-z0-9]/gm, '-').replace(/[-]{1,}/gm, '-');
+};
 
 const translitCyrToLat = () => {
   const editor = Window.activeTextEditor;
@@ -27,12 +46,31 @@ const translitCyrToLat = () => {
   });
 };
 
+const translitSlug = () => {
+  const editor = Window.activeTextEditor;
+  if (!editor) return;
+
+  const selections = editor.selections;
+
+  if (selections.length === 0) return;
+
+  editor.edit(editBuilder => {
+    selections.forEach(selectionItem => {
+      const text = editor.document.getText(selectionItem);
+      const convertedText = strToSlug(text);
+
+      editBuilder.replace(selectionItem, convertedText);
+    });
+  });
+};
+
 const reg = (name, callback) => {
   Commands.registerTextEditorCommand(`extension.${name}`, callback);
 };
 
 exports.activate = context => {
   context.subscriptions.push(reg('translitCyrToLat', translitCyrToLat));
+  context.subscriptions.push(reg('translitSlug', translitSlug));
 };
 
 exports.deactivate = () => {};
